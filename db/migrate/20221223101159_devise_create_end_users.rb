e1e9abdf55965c59779dbbfd0793800e3ef290d6
EC2 Default User <ec2-user@ip-172-31-23-26.ap-northeast-1.compute.internal>
2023-01-31 11:00:33 +0000

    deploy setting

--- db/migrate/20221223101159_devise_create_end_users.rb
+++ db/migrate/20221223101159_devise_create_end_users.rb
@@ -2,7 +2,7 @@
 
 class DeviseCreateEndUsers < ActiveRecord::Migration[6.1]
   def change
-    create_table :end_users do |t|
+    create_table :end_users, id: :integer do |t|
       ## Database authenticatable
       t.string  :email,                null: false, default: ""
       t.string :encrypted_password,   null: false, default: ""
